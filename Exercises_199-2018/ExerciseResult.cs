d5774b9aba74217a6e36ee826561e94338c5e15e
Zeljko199 <zeljko.milovanovic0611765038@gmail.com>
2020-11-09 12:43:33 +0100

    Kreirana je klasa sa potrebnim metodama

---
+++ Exercises_199-2018/ExerciseResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_199_2018
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+        
+        public ExerciseResult(int Id, string StudentName, string StudentIndex, int Points)
+        {
+            this.Id=Id;
+            this.StudentName= StudentName;
+            this.StudentIndex= StudentIndex;
+            this.Points= Points;
+        }
+
+        public override string ToString()
+        {
+            return Id+". " + StudentName + " ("+StudentIndex+") "+Points;
+        }
+    }
+}
